e69767cb80c486fa69afc8bc37fb16684740f4d6
cw <cw@truesightai.com>
2025-05-01 17:33:56 +0800

    test

--- db/batch.go
+++ db/batch.go
@@ -131,6 +131,7 @@ func (writeBatch *WriteBatch) Commit() error {
 		if !ok {
 			return ErrUpdateIndexFailed
 		}
+		
 		if oldValue != nil {
 			writeBatch.db.invalidSize += int64(oldValue.RecordSize)
 		}
